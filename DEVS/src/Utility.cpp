a3509ca5d7569011d0c74c471107e9c34683591c
rapsealk <piono623@naver.com>
2019-06-15 15:51:15 +0900

    initial commit

---
+++ DEVS/src/Utility.cpp
@@ -0,0 +1,6 @@
+#include "../include/Utility.hpp"
+
+void ClearMessageQueue(std::queue<std::string> &messageQueue) {
+	//std::queue<std::string> emptyQueue;
+	//std::swap(messageQueue, emptyQueue);
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DEVS: Discrete Event System Specification
+
+## Build
+```
+$ brew install cmake
+$ cmake CMakeLists.txt
+$ make
+```
